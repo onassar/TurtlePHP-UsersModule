757962ae5014a1b15385b5d9eee8fb595ac3700c
Oliver Nassar <onassar@gmail.com>
2015-01-07 11:11:21 -0500

    Copy

--- views/emails/bypassPassword.v1.inc.php
+++ views/emails/bypassPassword.v1.inc.php
@@ -3,10 +3,10 @@
     // namespaces
     namespace Modules\Users;
     $changePasswordPath = getConfig('paths', 'changePassword');
-?>Hi <?= ($user->firstName) ?>,
+?>Hi <?= ($user->firstName) ?>,<br /><br />
 
-Please follow this link to reset your password:
-<?= ($user->getAutoLoginUrl($changePasswordPath)) ?>
+Please follow this link to reset your password:<br />
+<?= ($user->getAutoLoginUrl($changePasswordPath)) ?><br /><br />
 
-Thanks,
+Thanks,<br />
 AccountDock
